4534f0af69566b7724e4a119ff399dd0d866a1c6
Samir Bhojwani <sbhojwani1@gmail.com>
2018-11-13 16:54:14 +0800

    Second

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,56 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show #
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to articles_path(@article)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+    redirect_to articles_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def edit_article_params
+    params.require(:article).permit(:title, :content)
+  end
+end
+
+
+             #       Prefix Verb   URI Pattern                                                       Controller#Action
+             #     articles GET    /articles(.:format)                                                                      articles#index
+             #              POST   /articles(.:format)                                                                      articles#create
+             #  new_article GET    /articles/new(.:format)                                                                  articles#new
+             # edit_article GET    /articles/:id/edit(.:format)                                                             articles#edit
+             #      article GET    /articles/:id(.:format)                                                                  articles#show
+             #              PATCH  /articles/:id(.:format)                                                                  articles#update
+             #              PUT    /articles/:id(.:format)                                                                  articles#update
+             #              DELETE /articles/:id(.:format)                                                                  articles#destroy
